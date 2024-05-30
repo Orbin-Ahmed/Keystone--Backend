a6d529d0f2d1175aec3e8657d15763dec6f13baa
Orbin-Ahmed <orbin.ahmed.acanto@g.bracu.ac.bd>
2024-05-30 18:50:25 +0400

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.getenv('DEBUG'))
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")
+ALLOWED_HOSTS = ["*"]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
